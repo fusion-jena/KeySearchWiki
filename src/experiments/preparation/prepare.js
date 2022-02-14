9bc7ece64e941d79258514197605e06fa67f7710
leila <leila.feddoul@uni-jena.de>
2022-02-14 15:21:24 +0100

    upgrade dependencies

--- src/experiments/preparation/prepare.js
+++ src/experiments/preparation/prepare.js
@@ -2,6 +2,12 @@ import getTargetEntities                   from './getTargetEntities';
 import getObjectDesc                       from './getObjectDesc';
 import toNTriples                          from './toNTriples';
 
+/**
+ * prepare the data to be indexed in a N-triples format
+ *
+ *
+ */
+
 (async function(){
 
   await getTargetEntities();
